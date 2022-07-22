97b02d281144705a5a3ca09fa5172c308b0c2fe4
Baitemir <baitemir1706@gmail.com>
2022-07-22 18:42:46 +0600

    responsive design added

--- src/WeatherInfo/Input.js
+++ src/WeatherInfo/Input.js
@@ -4,7 +4,7 @@ import classes from "./Input.module.css"
 const Input = ({getData}) => {
   const [value, setValue] = useState("")
   return ( <div className={classes.Input}>
-    <label htmlFor="city">City</label>
+    <label htmlFor="city">City:</label>
     <input type="text" name="city" id="city" onInput={(e)=> setValue(e.target.value)}/>
     <button onClick={()=>{getData(value)}}>Search</button>
   </div> );
